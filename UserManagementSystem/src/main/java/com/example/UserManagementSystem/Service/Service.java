e10d9ca26e4501a440484081e4510179188047e0
Praful <prafulpatil766254@gmail.com>
2023-06-23 15:25:31 +0530

    Added my project to master branch

---
+++ UserManagementSystem/src/main/java/com/example/UserManagementSystem/Service/Service.java
@@ -0,0 +1,61 @@
+package com.example.UserManagementSystem.Service;
+
+import com.example.UserManagementSystem.Entity.User;
+import com.example.UserManagementSystem.Repository.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@org.springframework.stereotype.Service
+public class Service {
+
+    @Autowired
+    Repository repository;
+
+
+    public String addUser(User user) {
+        List <User> users = repository.getListOfUsers();
+
+        users.add(user);
+        return "User is successfully added..";
+    }
+
+    public User getUserById(Integer id) {
+        List <User> users = repository.getListOfUsers();
+        for(User user : users){
+            if(user.getUserId().equals(id)){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public List<User> getAllUsers() {
+        List<User> users = repository.getListOfUsers();
+        return users;
+    }
+
+    public String updateUserInfo(User user) {
+        List<User> users = repository.getListOfUsers();
+
+        for(User presentUser : users){
+            if(presentUser.getUserId().equals(user.getUserId())){
+                users.remove(presentUser);
+                users.add(user);
+                break;
+            }
+        }
+        return "User Updated Successfully..";
+    }
+
+    public String deleteUser(Integer userId) {
+        List<User> users = repository.getListOfUsers();
+        for(User user : users){
+            if(user.getUserId().equals(userId)){
+                users.remove();
+                break;
+            }
+        }
+        return "User removed successfully..";
+    }
+}
